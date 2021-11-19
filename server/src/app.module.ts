3aed520fe282bab5f20e8493236879f81f0f3d54
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2021-11-19 08:39:21 -0500

    default server with mongoDB connection with X.509 Cert

---
+++ server/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING, {
+      tlsCertificateKeyFile: `${__dirname}/../X509-cert.pem`,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
